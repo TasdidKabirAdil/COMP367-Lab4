560310ac31fb902320ed1995d3c1613b578fc867
TasdidKabirAdil <tadil@my.centennialcollege.ca>
2025-03-26 14:43:40 -0400

    first commit

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UtilityLibraries
+{
+    public static class StringLibrary
+    {
+        public static bool StartWithUpper(this string str)
+        {
+            if(string.IsNullOrEmpty(str)) return false;
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
